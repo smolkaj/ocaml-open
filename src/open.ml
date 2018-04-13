97f8abb31879ee790cbd3a987c81d7898f830be0
Steffen Smolka <smolka@cs.cornell.edu>
2018-04-13 09:58:27 -0400

    return boolean flag indicating normal/abnormal exit; experimental support for Windows

--- src/open.ml
+++ src/open.ml
@@ -1,8 +1,12 @@
 type os =
   | MacOS
   | Linux
+  | Windows
+  | Cygwin
 
 let detect_os () : os =
+  if Sys.win32 then Windows else
+  if Sys.cygwin then Cygwin else
   let in_ch,_,_ as uname = Unix.open_process_full "uname" [||] in
   let os = input_line in_ch in
   ignore (Unix.close_process_full uname);
@@ -15,9 +19,16 @@ let open_cmd =
   match detect_os () with
   | MacOS -> "open"
   | Linux -> "xdg-open"
+  | Windows | Cygwin -> "start"
 
-let in_default_app file =
-  let silence = "> /dev/null 2>&1" in
+let silence =
+  match detect_os () with
+  | MacOS | Linux -> "> /dev/null 2>&1"
+  | Windows | Cygwin -> "> nul 2>&1"
+
+let in_default_app file : bool =
   Format.sprintf "%s %s %s" open_cmd file silence
   |> Unix.system
-  |> ignore
+  |> function
+    | Unix.WEXITED 0 -> true
+    | _ -> false
--- src/open.mli
+++ src/open.mli
@@ -1,2 +1,4 @@
-(** Opens the given file in the default app associated with the file's type. *)
-val in_default_app : string -> unit
+(** Opens the given file in the default app associated with the file's type.
+    Returns `true` if the open command exits normally, and `false` otherwise.
+  *)
+val in_default_app : string -> bool
